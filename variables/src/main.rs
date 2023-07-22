bcb82472f899d239c748b88f34689e0feee86e2a
swapnil <palash.swapnil@gmail.com>
2023-07-22 12:25:54 +0000

    ch3 funcs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,18 +1,8 @@
-use std::io;
-
-// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-    let mut index = String::new();
+    let x = plus_one(3);
+    println!("The value of x is {x}");
+}
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Not a valid index");
-
-    let index = index.trim().parse::<usize>().expect("Not a number");
-
-    println!(
-        "The val of chosen element at index {} is {}",
-        index, a[index],
-    );
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
